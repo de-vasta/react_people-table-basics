725d98efc3ff7cec65b5f90af1815fc6468ab355
Nazar Kundys <nazarrisd@gmail.com>
2026-02-08 21:29:12 +0200

    ✨ feat: implement React Router with navigation and page components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import PeoplePage from './pages/PeoplePage';
+import NotFound from './pages/NotFound';
+import HomePage from './pages/HomePage';
+import { AppPath } from './types/paths';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path={AppPath.Home} element={<Navigate to="/" replace />} />
+        <Route path={AppPath.People} element={<PeoplePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   </Router>,
 );
---
+++ src/types/paths.ts
@@ -0,0 +1,4 @@
+export enum AppPath {
+  Home = '/home',
+  People = '/people',
+}
